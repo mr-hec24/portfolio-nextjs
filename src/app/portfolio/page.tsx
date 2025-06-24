4a7c1351c7e34d0827f904b869b70250ddf12ca9
Hector Armando Rodriguez <harmandorod@hotmail.com>
2025-06-24 04:38:37 -0600

    Changed string[] to string
    Cause why not

--- src/app/portfolio/page.tsx
+++ src/app/portfolio/page.tsx
@@ -33,7 +33,7 @@ interface ProjectDetail {
 
 // Define the correct props type for a dynamic page component in Next.js App Router
 // This is the most common and robust way to type it.
-type ProjectDetailPageProps = Promise<{slug: string[]}>;
+type ProjectDetailPageProps = Promise<{slug: string}>;
   // searchParams can be an object with string keys and values that are string or array of strings, or undefined.
   //searchParams?: { [key: string]: string | string[] | undefined };
 
